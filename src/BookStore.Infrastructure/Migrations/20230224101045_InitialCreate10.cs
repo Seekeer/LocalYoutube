c872080638eef17eeff990d9f9702166993487ce
Dim <seekeer@yandex.ru>
2023-02-28 12:35:31 +0300

    DB update

---
+++ src/BookStore.Infrastructure/Migrations/20230224101045_InitialCreate10.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoFileUserInfos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoFileUserInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VideoFileId = table.Column<int>(type: "int", nullable: false),
+                    Position = table.Column<double>(type: "float", nullable: false),
+                    Rating = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoFileUserInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VideoFileUserInfos_DbFile_VideoFileId",
+                        column: x => x.VideoFileId,
+                        principalTable: "DbFile",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoFileUserInfos_VideoFileId",
+                table: "VideoFileUserInfos",
+                column: "VideoFileId",
+                unique: true);
+        }
+    }
+}
---
+++ src/BookStore.Infrastructure/Migrations/20230224105856_InitialCreate11.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VideoFileUserInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    VideoFileId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Position = table.Column<double>(type: "float", nullable: false),
+                    Rating = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoFileUserInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VideoFileUserInfos_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_VideoFileUserInfos_DbFile_VideoFileId",
+                        column: x => x.VideoFileId,
+                        principalTable: "DbFile",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoFileUserInfos_UserId",
+                table: "VideoFileUserInfos",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoFileUserInfos_VideoFileId",
+                table: "VideoFileUserInfos",
+                column: "VideoFileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoFileUserInfos");
+        }
+    }
+}
