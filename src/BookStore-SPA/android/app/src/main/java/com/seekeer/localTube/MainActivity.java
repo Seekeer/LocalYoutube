e0f4f943e040626e5b513f43d25552e572da1925
Dim <seekeer@yandex.ru>
2023-11-10 22:56:14 +0300

    capacitor initial

---
+++ src/BookStore-SPA/android/app/src/main/java/com/seekeer/localTube/MainActivity.java
@@ -0,0 +1,5 @@
+package com.seekeer.localTube;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/BookStore-SPA/capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.seekeer.localTube',
+  appName: 'LocalTube',
+  webDir: 'dist/BookStore-SPA',
+};
+
+export default config;
