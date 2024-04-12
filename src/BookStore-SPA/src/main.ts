68c09e25cc5735a3015c1cdbbcaf21da1d707c78
Dim <seekeer@yandex.ru>
2024-04-12 18:16:00 +0300

    Add Sentry Logs

--- src/BookStore-SPA/src/main.ts
+++ src/BookStore-SPA/src/main.ts
@@ -1,12 +1,40 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+import * as Sentry from '@sentry/angular-ivy';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+Sentry.init({
+  dsn: "https://7b5242a17c30c84cd9c3f155eb7d296a@o4507070039851008.ingest.de.sentry.io/4507070043455568",
+  integrations: [
+    // Registers and configures the Tracing integration,
+    // which automatically instruments your application to monitor its
+    // performance, including custom Angular routing instrumentation
+    Sentry.browserTracingIntegration(),
+    // Registers the Replay integration,
+    // which automatically captures Session Replays
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^http:\/\/192.168.1.55/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
